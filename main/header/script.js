4c4695f6afe5f43c2d85cb6734556d2428f27178
hayu yonatan <hayuyonatan@gmail.com>
2025-03-15 10:45:42 +0300

    Delete main directory
    Signed-off-by: hayu yonatan <hayuyonatan@gmail.com>

--- main/header/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navLinks = document.getElementById("nav-links");
-
-  menuToggle.addEventListener("click", function () {
-    navLinks.classList.toggle("nav-active");
-  });
-});
